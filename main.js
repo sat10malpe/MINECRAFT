d90657a6c9e3a09cacffa96104e02db2cf5353c5
sat10malpe <sathyavantha.bhat@gmail.com>
2022-02-04 19:50:48 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -130,3 +130,47 @@ if(keyPressed == '67')
     console.log("c");
 }
 }
+function up()
+{
+    if(player_y>=0)
+    {
+        player_y = player_y - block_image_height;
+        console.log("block image height =" + block_image_height);
+        console.log("when Up arrow key is pressed, X = "+ player_x +",Y ="+player_y)
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function down()
+{
+    if(player_y <=500)
+    {
+        player_y = player_y + block_image_height;
+        console.log("block image height =" + block_image_height);
+        console.log("when Down arrow key is pressed, X = "+ player_x +",Y ="+player_y)
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function left()
+{
+    if(player_x >0)
+    {
+        player_x = player_x - block_image_width;
+        console.log("block image width =" + block_image_width);
+        console.log("when Left arrow key is pressed, X = "+ player_x +",Y ="+player_y)
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function right()
+{
+    if(player_x <=850)
+    {
+        player_x = player_x + block_image_width;
+        console.log("block image width =" + block_image_width);
+        console.log("when Right arrow key is pressed, X = "+ player_x +",Y ="+player_y)
+        canvas.remove(player_object);
+        player_update();
+    }
+}
